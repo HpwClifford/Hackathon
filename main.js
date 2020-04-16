4f0cb2b7b36e6920141344d8b934a9e8f3d3865c
Kim Wysocki <kcwysocka@icloud.com>
2020-04-15 23:24:02 -0400

    hit MVPs

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-class BitCoinAppInstance {
+class AppInstance {
     constructor () {
         this.check();
         this.checkCount = 0;
-
+        this.user = {'buyPrice': undefined, 'sellPrice': undefined};
+        this.alertedUser = false;
+        
         // start continuosly cehcking BTC price
         setInterval(this.check.bind(this), 3000);
-
     }
 
     // methods...
@@ -15,18 +16,88 @@ class BitCoinAppInstance {
         fetch("https://api.coinbase.com/v2/prices/spot?currency=USD")
         .then((data) => data.json())
         .then((data) => {
-            console.log(this.checkCount, data.data.amount);
+            let price = data.data.amount;
+            this.update(price);
+            console.log(this.checkCount, price, this.user);
+
+            // Check if price is currently higher than sell price 
+            // (below -> or lower than buy price)
+            if (price >= this.user.sellPrice && !this.alertedUser) {
+                alert("SELL NOW!");
+                this.alertedUser = true;
+            } else if (price <= this.user.buyPrice && !this.alertedUser) {
+                alert("BUY NOW!");
+                this.alertedUser = true;
+            }
+
+            if (this.user.sellPrice > price && price > this.user.buyPrice) {
+                this.alertedUser = false;
+            }
         });
     }
 
-    userInput () {
+    update (price) {
+        // create new html element to store price
+        // append to body
+        const priceInfo = document.getElementById("current-price");
+        priceInfo.innerHTML = `$${price}`;
+        
+    }
 
+    inputError () {
+        console.log('error!!!');
+        const errorMessage = document.getElementById("error-message");
+        errorMessage.style.visibility = "visible";
+        setTimeout(() => errorMessage.style.visibility = "hidden", 3000);
+    }
+
+    areYouSureError () {
+        const errorMessage = document.getElementById("are-you-sure");
+        errorMessage.style.visibility = "visible";
+        setTimeout(() => errorMessage.style.visibility = "hidden", 3000);
     }
 }
 
-// $(document).ready(() => {
-    // call constructor
-    
-// });
+// program initialization
+document.addEventListener("DOMContentLoaded", function() {
+    // Handler when the DOM is fully loaded
+    const instance = new AppInstance();
 
-new BitCoinAppInstance();
+    const btn = document.getElementById("submit");
+
+    btn.addEventListener("click", () => {
+        // storing buy / sell values
+        const buyValue = document.getElementById("buyPrice").value;
+        const sellValue = document.getElementById("sellPrice").value;
+        console.log(typeof buyValue);
+
+        // if inputs valid
+        if (!isNaN(buyValue) 
+            && !isNaN(sellValue) 
+            && Number(buyValue) >= 0 
+            && Number(sellValue) >= 0) {
+
+            if (Number(buyPrice) === Number(sellPrice)) {
+                console.log('helloooo');
+                instance.inputError();
+            }
+
+            // input into user obj
+            instance.user.buyPrice = buyValue;
+            instance.user.sellPrice = sellValue;
+
+        // if inputs invalid
+        } else {
+            instance.inputError();
+        }
+
+        // clear inputs
+        document.getElementById("buyPrice").value = null;
+        document.getElementById("sellPrice").value = null;
+    });
+
+});
+
+
+
+
